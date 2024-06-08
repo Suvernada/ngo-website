ed6a6ad2b659a480e2509c31909115eeb157137a
Suvernada <140585225+Suvernada@users.noreply.github.com>
2024-06-08 19:59:30 +0530

    Update CausesPage.jsx

--- src/pages/CausesPage.jsx
+++ src/pages/CausesPage.jsx
@@ -6,161 +6,68 @@ import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 
 const CausesPage = () => {
-  return (
-    <>
-      <Header />
-      <section className="causes-section" style={{padding:"30px 0"}}>
-        <div className="container">
-          <div className="section-heading text-center mb-4">
-            <h2>Platforms</h2>
-            <span className="heading-border"></span>
-            <p>
-              Help today because tomorrow you may be the one who <br /> needs
-              more helping!
-            </p>
-          </div>
-          <div className="row align-items-center" style={{ padding: "50px 0" }}>
-            <div className="col-lg-6 col-md-12">
-              <div className="section-heading text-left mb-4">
-                <h2>Collaboratory for Sustainable Development</h2>
-                <span className="heading-border"></span>
-              </div>
-              <p>
-                The primary aim of the 'Collaboratory for Sustainable
-                Development' is to initiate and conduct cutting-edge research
-                that addresses knowledge gaps and produces actionable plans for
-                sustainable development. Our team consists of highly skilled and
-                experienced researchers, including accomplished PhDs and PhD
-                scholars. Currently, our research efforts are concentrated in
-                key areas such as behavioral economics, development economics,
-                agricultural economics, and food and agricultural policy.
-                </p><p>
-                We are committed to maintaining a rigorous approach to
-                developing research proposals, ensuring the highest quality in
-                our research endeavors. Moreover, ethical considerations hold
-                significant importance in all aspects of our work, ensuring that
-                our research is not only impactful but also socially
-                responsible.
-                </p><p>
-                In addition to research, we actively engage in knowledge-sharing
-                and capacity-building activities. This includes hosting
-                seminars, organizing training sessions, and conducting workshops
-                where experts and stakeholders come together to brainstorm ideas
-                and enhance their understanding of sustainable development
-                issues.
-                </p><p>
-                By combining our expertise, diverse perspectives, and
-                collaborative spirit, we strive to contribute meaningfully to
-                the advancement of sustainable development and foster positive
-                change in our society and the world at large.
-              </p>
-            </div>
-            <div className="col-lg-6 col-md-12 text-center">
-              <img src={img1} className="img-fluid" alt="director-1" />
-            </div>
-          </div>
-          <div className="row align-items-center" style={{ padding: "50px 0" }}>
-            <div className="col-lg-6 col-md-12 text-center">
-              <img src={img2} className="img-fluid" alt="director-2" />
-            </div>
-            <div className="col-lg-6 col-md-12">
-              <div className="section-heading text-left mb-4">
-                <h2>Harmonious Nourishment Initiative</h2>
-                <span className="heading-border"></span>
-              </div>
-              <p>
-                Through the 'Harmonious Nourishment Initiative,' we actively
-                advocate for the consumption of millet food products while
-                providing comprehensive services in nutrition counseling and
-                therapy. Our initiative is centered around fostering a
-                harmonious relationship with nourishment, emphasizing the
-                importance of incorporating millet into one's dietary choices.
-                </p><p>
-                At our initiative, we actively promote millet food products,
-                recognizing their immense nutritional value and their
-                contribution to overall well-being. We strive to raise awareness
-                about the benefits of millet and its role in supporting a
-                healthy and balanced diet. By highlighting the diverse range of
-                millet-based food options available, we encourage individuals to
-                make informed choices that prioritize their well-being and
-                promote sustainable food practices.
-                </p><p>
-                In addition to promoting millet-based food products, our
-                services extend to nutrion counseling. We understand that each
-                individual has unique dietary needs and goals, and our
-                experienced team of professionals is dedicated to providing
-                tailored guidance and support. Through our nutrition counseling
-                sessions, we aim to empower individuals with knowledge and
-                strategies to make informed decisions about their dietary
-                habits, enabling them to achieve optimal health and wellness.
-                </p><p>
-                Furthermore, our initiative offers comprehensive diet therapy
-                services. We recognize the potential impact of diet on various
-                health conditions and concerns, and our therapy programs are
-                designed to address specific dietary requirements for
-                individuals with specific needs. By employing evidence-based
-                approaches and personalized meal plans, we strive to support
-                individuals on their journey toward improved health and overall
-                well-being
-                </p><p>
-                Under the 'Harmonious Nourishment Initiative,' we are committed
-                to promoting millet food products, providing expert nutrition
-                counseling to foster a harmonious and balanced approach to
-                nourishment. Our goal is to empower individuals to make mindful
-                choices, embrace the benefits of millet, and optimize their
-                health through holistic dietary practices.
-              </p>
-            </div>
-          </div>
-          <div className="row align-items-center" style={{ padding: "50px 0" }}>
-            <div className="col-lg-6 col-md-12">
-              <div className="section-heading text-left mb-4">
-                <h2>The Empowered Minds Studio</h2>
-                <span className="heading-border"></span>
-              </div>
-              <p>
-                At 'The Empowered Minds Studio,' we specialize in various
-                activities aimed at personal growth and creativity. Our team of
-                passionate individuals engages in a range of endeavors,
-                including crafting insightful self-help blogs, conducting
-                creative writing classes, and providing mentorship to children
-                and young adults, nurturing their potential as budding authors.
-                </p><p>
-                Within our vibrant studio, we cultivate an environment that
-                encourages individuals to explore their inner strengths, develop
-                their writing skills, and embrace the power of storytelling.
-                Through our self-help blogs, we strive to empower and inspire
-                readers with valuable insights and practical guidance for
-                personal transformation. Our creative writing classes offer a
-                dynamic and supportive setting where aspiring writers can
-                unleash their imagination, refine their craft, and cultivate
-                their unique writing styles. We foster a nurturing atmosphere
-                that nurtures creativity, encourages self-expression, and equips
-                individuals with the tools and techniques to effectively
-                communicate their ideas.
-                </p><p>
-                Additionally, we take great pride in our mentorship program,
-                where we guide and inspire children and young adults to embark
-                on their writing journeys and become book authors at an early
-                age. Through personalized guidance, we help them unlock their
-                creative potential, cultivate a love for storytelling, and
-                instill confidence in their writing abilities.
-                </p><p>
-                At 'The Empowered Minds Studio,' we are passionate about
-                empowering individuals to embrace their creativity, discover
-                their unique voices, and make a positive impact through the art
-                of writing.
-              </p>
-            </div>
-            <div className="col-lg-6 col-md-12 text-center">
-              <img src={img3} className="img-fluid" alt="director-1" />
-            </div>
-          </div>
-        </div>
-      </section>
-      <Footer/>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <section className="causes-section" style={{ padding: "30px 0" }}>
+                <div className="container">
+                    <div className="section-heading text-center mb-4">
+                        <h2>Platforms</h2>
+                        <span className="heading-border"></span>
+                        <p>
+                            Help today because tomorrow you may be the one who <br /> needs more helping!
+                        </p>
+                    </div>
+                    <div className="row align-items-center" style={{ padding: "50px 0" }}>
+                        <div className="col-lg-6 col-md-12">
+                            <div className="section-heading text-left mb-4">
+                                <h2>Collaboratory for Sustainable Development</h2>
+                                <span className="heading-border"></span>
+                            </div>
+                            <p>The primary aim of the 'Collaboratory for Sustainable Development' is to initiate and conduct cutting-edge research that addresses knowledge gaps and produces actionable plans for sustainable development. Our team consists of highly skilled and experienced researchers, including accomplished PhDs and PhD scholars. Currently, our research efforts are concentrated in key areas such as behavioral economics, development economics, agricultural economics, and food and agricultural policy.</p>
+                            <p>We are committed to maintaining a rigorous approach to developing research proposals, ensuring the highest quality in our research endeavors. Moreover, ethical considerations hold significant importance in all aspects of our work, ensuring that our research is not only impactful but also socially responsible.</p>
+                            <p>In addition to research, we actively engage in knowledge-sharing and capacity-building activities. This includes hosting seminars, organizing training sessions, and conducting workshops where experts and stakeholders come together to brainstorm ideas and enhance their understanding of sustainable development issues.</p>
+                            <p>By combining our expertise, diverse perspectives, and collaborative spirit, we strive to contribute meaningfully to the advancement of sustainable development and foster positive change in our society and the world at large.</p>
+                        </div>
+                        <div className="col-lg-6 col-md-12 text-center">
+                            <img src={img1} className="img-fluid" alt="director-1" />
+                        </div>
+                    </div>
+                    <div className="row align-items-center" style={{ padding: "50px 0" }}>
+                        <div className="col-lg-6 col-md-12 text-center">
+                            <img src={img2} className="img-fluid" alt="director-2" />
+                        </div>
+                        <div className="col-lg-6 col-md-12">
+                            <div className="section-heading text-left mb-4">
+                                <h2>Harmonious Nourishment Initiative</h2>
+                                <span className="heading-border"></span>
+                            </div>
+                            <p>Through the 'Harmonious Nourishment Initiative,' we actively advocate for the consumption of millet food products while providing comprehensive services in nutrition counseling and therapy. Our initiative is centered around fostering a harmonious relationship with nourishment, emphasizing the importance of incorporating millet into one's dietary choices.</p>
+                            <p>At our initiative, we actively promote millet food products, recognizing their immense nutritional value and their contribution to overall well-being. We strive to raise awareness about the benefits of millet and its role in supporting a healthy and balanced diet. By highlighting the diverse range of millet-based food options available, we encourage individuals to make informed choices that prioritize their well-being and promote sustainable food practices.</p>
+                            <p>In addition to promoting millet-based food products, our services extend to nutrion counseling. We understand that each individual has unique dietary needs and goals, and our experienced team of professionals is dedicated to providing tailored guidance and support. Through our nutrition counseling sessions, we aim to empower individuals with knowledge and strategies to make informed decisions about their dietary habits, enabling them to achieve optimal health and wellness.</p>
+                            <p>Furthermore, our initiative offers comprehensive diet therapy services. We recognize the potential impact of diet on various health conditions and concerns, and our therapy programs are designed to address specific dietary requirements for individuals with specific needs. By employing evidence-based approaches and personalized meal plans, we strive to support individuals on their journey toward improved health and overall well-being</p>
+                            <p>Under the 'Harmonious Nourishment Initiative,' we are committed to promoting millet food products, providing expert nutrition counseling to foster a harmonious and balanced approach to nourishment. Our goal is to empower individuals to make mindful choices, embrace the benefits of millet, and optimize their health through holistic dietary practices.</p>
+                        </div>
+                    </div>
+                    <div className="row align-items-center" style={{ padding: "50px 0" }}>
+                        <div className="col-lg-6 col-md-12">
+                            <div className="section-heading text-left mb-4">
+                                <h2>The Empowered Minds Studio</h2>
+                                <span className="heading-border"></span>
+                            </div>
+                            <p>At 'The Empowered Minds Studio,' we specialize in various activities aimed at personal growth and creativity. Our team of passionate individuals and teachers engages in a range of endeavors, including crafting insightful self-help blogs, conducting creative writing classes, and providing mentorship to children and young adults to nurture their potential.</p>
+                            <p>Dawn Blossom Club (www.dawnblossomclub.com) is a part of The Empowered Minds Studio that runs online activities for exploring inner strengths through music, journaling, art, creative writing etc. for kids and adults.</p>
+                            <p>The Bright Words Project ( https://www.dawnblossomclub.com/blog/categories/bright-words-project ) is an ongoing project of Dawn Blossom Club for showcasing selected entries (articles/poems/short stories) conveying optimism and hope. The entries are published on Dawn Blossom Blog. Dawn Blossom Club encourages people from all walks of life to write and share their light of optimism and hope (https://www.dawnblossomclub.com/blank-3).</p>
+                        </div>
+                        <div className="col-lg-6 col-md-12 text-center">
+                            <img src={img3} className="img-fluid" alt="director-1" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    );
 };
 
 export default CausesPage;
